a9d772f5890aa4c20d83aef371b403a449f2efc8
Bary Pollack <bary.pollack@baker.edu>
2021-08-31 23:15:02 -0700

    Commit all files

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 4f;
+    [SerializeField]
+    private GameObject _laserPrefab;
+
+    private Player _player;
+    private Animator _anim;
+    private AudioSource _audioSource;
+    private float _fireRate = 3f;
+    private float _canFire = -1f;
+
+    void Start()
+    {
+        _player = GameObject.Find("Player").GetComponent<Player>();
+        _audioSource = GetComponent<AudioSource>();
+
+        if (_player == null)
+        {
+            Debug.LogError("The Player is NULL");
+        }
+        _anim = GetComponent<Animator>();
+
+        if (_anim == null)
+        {
+            Debug.LogError("Animator is NULL");
+        }
+    }
+
+    void Update()
+    {
+        CalculateMovement();
+
+        if (3>2)      //FOO Disable Enemy lasers
+        if (Time.time > _canFire)
+        {
+            _fireRate = Random.Range(3f, 7f);
+            _canFire = Time.time + _fireRate;
+            GameObject enemyLaser = Instantiate(_laserPrefab, transform.position, Quaternion.identity);
+            Laser[] lasers = enemyLaser.GetComponentsInChildren<Laser>();
+
+            for (int i = 0; i < lasers.Length; i++)
+            {
+                lasers[i].AssignEnemyLaser();
+            }
+        }
+    }
+
+    void CalculateMovement()
+    {
+        transform.Translate(_speed * Time.deltaTime * Vector3.down);
+        if (transform.position.y < -4f)
+        {
+            float randomX = Random.Range(-8f, 8f);
+            transform.position = new Vector3(randomX, 7f, 0);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Player player = other.transform.GetComponent<Player>();
+            if (player != null)
+            {
+                player.Damage();
+            }
+
+            _anim.SetTrigger("OnEnemyDeath");
+            _speed = 0;
+            _audioSource.Play();
+            Destroy(this.gameObject, 2.8f);
+        }
+
+        if (other.tag == "Laser")
+        {
+            Destroy(other.gameObject);
+
+            if (_player != null)
+            {
+                _player.AddScore(10);
+            }
+
+            _anim.SetTrigger("OnEnemyDeath");
+            _speed = 0;
+            _audioSource.Play();
+
+            Destroy(GetComponent<Collider2D>());
+            Destroy(this.gameObject, 2.8f);
+        }
+    }
+}
---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 8f;
+    private bool _isEnemyLaser = false;
+
+    void Update()
+    {
+        if (!_isEnemyLaser || true)    //FOO Disable Enemy lasers
+            MoveUp();
+        else
+            MoveDown();
+    }
+
+    void MoveUp()
+    {
+        transform.Translate(_speed * Time.deltaTime * Vector3.up);
+        if (transform.position.y > 8f)
+        {
+            if (transform.parent != null)
+            {
+                Destroy(transform.parent.gameObject);
+            }
+            Destroy(this.gameObject);
+        }
+    }
+    void MoveDown()
+    {
+        transform.Translate(_speed * Time.deltaTime * Vector3.down);
+        if (transform.position.y < -8f)
+        {
+            if (transform.parent != null)
+            {
+                Destroy(transform.parent.gameObject);
+            }
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void AssignEnemyLaser()
+    {
+        _isEnemyLaser = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player" && _isEnemyLaser)
+        {
+            Player player = other.GetComponent<Player>();
+            if (player != null)
+                player.Damage();
+        }
+    }
+}
